d9644dd4bb2953a580f59fdd700813de5a9a9a89
lea.arturi <lea.arturi@gmail.com>
2021-09-18 14:38:39 -0300

    Templates

--- ProyectoWeb/ProyectoWeb/urls.py
+++ ProyectoWeb/ProyectoWeb/urls.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-
-from ProyectoWebApp import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="Home"),
-    path('servicios', views.servicios, name="Servicios"),
-    path('tienda', views.tienda, name="Tienda"),
-    path('blog', views.blog, name="Blog"),
-    path('contacto', views.contacto, name="Contacto"),
+    path('', include('ProyectoWebApp.urls')),
 ]
--- ProyectoWeb/ProyectoWebApp/views.py
+++ ProyectoWeb/ProyectoWebApp/views.py
@@ -3,24 +3,24 @@ from django.shortcuts import render, HttpResponse
 
 def home(request):
 
-    return HttpResponse("Inicio")
+    return render(request, "ProyectoWebApp/home.html")
 
 
 def servicios(request):
 
-    return HttpResponse("Servicios")
+    return render(request, "ProyectoWebApp/servicios.html")
 
 
 def tienda(request):
 
-    return HttpResponse("Tienda")
+    return render(request, "ProyectoWebApp/tienda.html")
 
 
 def blog(request):
 
-    return HttpResponse("Blog")
+    return render(request, "ProyectoWebApp/blog.html")
 
 
 def contacto(request):
 
-    return HttpResponse("Contacto")
+    return render(request, "ProyectoWebApp/contacto.html")

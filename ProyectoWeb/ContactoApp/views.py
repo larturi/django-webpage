8e3e9938e4a6b7e346a8054f6da57fa5d1da1a0b
lea.arturi <lea.arturi@gmail.com>
2021-09-23 15:01:37 -0300

    Envio del POST con los datos del formulario

--- ProyectoWeb/ContactoApp/views.py
+++ ProyectoWeb/ContactoApp/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .forms import FormContacto
 
 def contacto(request):
 
     formulario_contacto = FormContacto()
+
+    if request.method == "POST":
+
+        formulario_contacto = FormContacto(data=request.POST)
+
+        if formulario_contacto.is_valid():
+
+            nombre = request.POST.get('nombre')
+            email = request.POST.get('email')
+            mensaje = request.POST.get('mensaje')
+
+            return redirect("/contacto/?valid")
 
     return render(request, "ContactoApp/contacto.html", {'formulario_contacto': formulario_contacto})
